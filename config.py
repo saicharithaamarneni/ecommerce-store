10ede172ccee44d5e655b14f2749c2c7a75c19d6
Your Name <saicharithaamarneni11@gmail.com>
2026-05-22 16:30:02 +0530

    Fix SQLite for Vercel

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
+import os
+
+BASE_DIR = "/tmp"
+
+
 class Config:
 
-    SECRET_KEY = "ecommerce123"
+    SECRET_KEY = "secret123"
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///ecommerce.db"
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" +
+        os.path.join(
+            BASE_DIR,
+            "ecommerce.db"
+        )
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
